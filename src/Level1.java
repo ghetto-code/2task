e1a1eba0379a4abaea8707715916196c8d486921
ghetto-code <85032385+ghetto-code@users.noreply.github.com>
2021-05-30 11:45:07 +0300

    Update Level1.java

--- src/Level1.java
+++ src/Level1.java
@@ -1,5 +1,5 @@
 public class Level1 {
-    public static int odometr(int[] oksana) {
+    public static int odometer(int[] oksana) {
         int distance = 0;
         if (oksana.length >= 2) {
             for (int i = 0; i < oksana.length; i += 2) {
