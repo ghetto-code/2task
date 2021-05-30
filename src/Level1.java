e0377419c880adf3b6a15b40dc5ab45c8318a319
c3po <bettercallanzor@gmail.com>
2021-05-30 11:17:37 +0300

    Initial commit

---
+++ src/Level1.java
@@ -0,0 +1,11 @@
+public class Level1 {
+    public static int odometr(int[] oksana) {
+        int distance = 0;
+        if (oksana.length >= 2) {
+            for (int i = 0; i < oksana.length; i += 2) {
+                distance += oksana[i] * oksana[i + 1];
+            }
+        }
+        return distance;
+    }
+}
